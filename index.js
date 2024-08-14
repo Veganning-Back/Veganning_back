552cd541223dc378218e94edaa3827a96eb4f705
wosyh18 <wosyh18@naver.com>
2024-08-14 16:41:29 +0900

    라우터 설정

---
+++ index.js
@@ -0,0 +1,31 @@
+// index.js
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+import express from "express";
+import { recipeRouter } from "./src/routes/recipes.route.js";
+import { recipeReviewRouter } from "./src/routes/recipesReview.route.js"
+import dbConfig from "./congfig/db.config.js";
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// 미들웨어 설정
+app.use(express.json()); // JSON 파싱 미들웨어
+
+
+// 디버깅 로그 추가
+app.use((req, res, next) => {
+  console.log(`Incoming request: ${req.method} ${req.url}`);
+  next();
+});
+
+// 라우터 설정
+app.use("/recipes", recipeRouter);
+
+app.use("/recipes", recipeReviewRouter);
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
