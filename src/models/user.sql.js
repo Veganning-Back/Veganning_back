287dd91741b12cdd4097f94dd1758e13db634c61
kongsumin <sm3904@naver.com>
2024-08-16 16:06:24 +0900

    db연결2

--- src/models/user.sql.js
+++ src/models/user.sql.js
@@ -3,10 +3,10 @@
 
 export const getUserID = "SELECT * FROM user WHERE user_id = ?";
 
-export const getUserSave_Rec = "SELECT * FROM recipe_savening WHERE user_id = ?";
-export const getUserSave_Res = "SELECT * FROM store_savening WHERE user_id = ?";
-export const deleteSavedRecipeQuery = `DELETE FROM recipe_savening WHERE user_id = ? AND recipe_id = ?`;
-export const deleteSavedRestaurantQuery = `DELETE FROM store_savening WHERE user_id = ? AND store_id = ?`;
+export const getUserSave_Rec = "SELECT * FROM recipe_savning WHERE user_id = ?";
+export const getUserSave_Res = "SELECT * FROM store_savning WHERE user_id = ?";
+export const deleteSavedRecipeQuery = `DELETE FROM recipe_savning WHERE user_id = ? AND recipe_id = ?`;
+export const deleteSavedRestaurantQuery = `DELETE FROM store_savning WHERE user_id = ? AND store_id = ?`;
 
 export const insertUserSql = `
     INSERT INTO user (email, password, name, start_vegan, signupdate, created_at, updated_at)
