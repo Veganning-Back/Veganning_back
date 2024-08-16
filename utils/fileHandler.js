3ad4effa2ae06e50fb18cdd34e1cafedf7a149c0
wosyh18 <wosyh18@naver.com>
2024-08-16 11:14:22 +0900

    이미지 추가 오류 수정

--- utils/fileHandler.js
+++ utils/fileHandler.js
@@ -1,15 +1,14 @@
 import path from "path";
 import fs from "fs/promises";
+//이미지 파일 -> 바이너리 
 
-const IMAGE_BASE_PATH = "/path/to/image/directory";
+export const readImageFile = async(filePath) => {
 
-export const readImageFile = async (filename) => {
-  const fullPath = path.join(IMAGE_BASE_PATH, filename);
-  try {
-    const data = await fs.readFile(fullPath);
-    return data;
-  } catch (error) {
-    console.error(`Error reading image file: ${fullPath}`, error);
-    throw error;
-  }
-};
+   try {
+      const data = await fs.readFile(filePath);
+      return data;
+   } catch (error) {
+      console.error(`Error reading image file: ${filePath}`, error);
+      throw error;
+   }
+   };
